3b5c1f2584521887854865b841bc0b74f6f3faf3
padraigfl <padraigflood1@gmail.com>
2019-04-23 01:03:55 +0100

    initial boilerplate

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
